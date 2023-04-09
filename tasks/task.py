de33b90c57e753427333fb6ac7680bf20e9bb183
Gruzin <gruzd.roman@inbox.ru>
2023-04-09 19:05:12 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,13 +1,26 @@
 from typing import Dict
+import time
 
 execution_time: Dict[str, float] = {}
 
 
 def time_decorator(fn):
-    """
-    Create a decorator function `time_decorator`
-    which has to calculate decorated function execution time
-    and put this time value to `execution_time` dictionary where `key` is
-    decorated function name and `value` is this function execution time.
-    """
-    pass
+    def wrapper(*args, **kwargs):
+        global execution_time
+        time1 = time.time()
+        result = fn(*args, **kwargs)
+        time2 = time.time()
+        time3 = time2 - time1
+        execution_time[fn.__name__] = time3
+        return result
+
+    return wrapper
+
+@time_decorator
+def func_add(a, b, sleep_time):
+    time.sleep(sleep_time)
+    print(a + b)
+
+sleep_time = 0.1
+func_add(10, 20, sleep_time)
+print(execution_time['func_add'])
